894c8008197c8e663afc86734d2c7c1b972ae45a
Plamen pandev <looopooo45@gmail.com>
2024-03-30 18:23:18 +0200

    Add project files.

---
+++ BadWords/CheckComments.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadWords
+{
+    public class CheckComments
+    {
+        private static Dictionary<string, bool> flagedWords = new Dictionary<string, bool>();
+        private static List<string> Init_Dict(string folder)
+        {
+            List<string> folderContent = new List<string>();
+            foreach (var directory in Directory.GetDirectories(folder))
+            {
+                folderContent.Add(directory);
+                folderContent.AddRange(Init_Dict(directory));
+
+            }
+            return folderContent;
+        }
+        public static void test(string path, List<string> comments)
+        {
+            List<string> folders = Init_Dict(path);
+            foreach (string folder in folders)
+            {
+                foreach (string filepath in Directory.GetFiles(folder, "*.txt"))// всчики файлове в папката
+                {
+                    string[] fileContent = File.ReadAllLines(filepath);
+                    for (int i = 0; i < fileContent.Length; i++)
+                    {
+                        try
+                        {
+                            flagedWords.Add(fileContent[i], true);
+                        }
+                        catch
+                        {
+                            // Console.WriteLine("this word already exist:", fileContent[i]);
+                        }
+                    }
+                }
+            }
+
+            foreach(string comment in comments)
+            {
+                string[] wordsInComment = comment.Split(new char[] { ',', ' '});
+                foreach (string word in wordsInComment)
+                {
+                    try
+                    {
+                        flagedWords.Add(word, true);   
+                    }
+                    catch
+                    {
+                        Console.WriteLine(comment);
+                        break;
+                    }
+                }
+            }
+        }
+    }
+}
---
+++ BadWords/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BadWords
+{
+    internal class Program:CheckComments
+    {
+        static void Main(string[] args)
+        {
+            string path = @"C:\Users\PlamenPandev\Desktop\BadWords\BadWords";
+            List<string> comments = new List<string>()
+            {
+                "everything is fine",
+                "руски комент гандон ",
+                "buceta",
+                "нещо си pillu",
+                "ich bin neshto si"
+            };
+            CheckComments.test(path, comments);
+        }
+    }
+}
